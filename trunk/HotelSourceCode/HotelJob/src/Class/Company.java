6a3195ce0aabde0a99ed5fd2a926b001a42b1838
quangtv61078@gmail.com <quangtv61078@gmail.com@15badab4-6e58-07c6-722d-240f7b262c5d>
2013-11-04 11:17:50 +0000

    Update script.sql version 3.0 Source code java

---
+++ trunk/HotelSourceCode/HotelJob/src/Class/Company.java
@@ -0,0 +1,140 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Class;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author quang
+ */
+public class Company implements Serializable {
+    private String companyID;
+    private String companyName;
+    private String information;
+    private String address;
+    private String contact;
+    private String phone;
+    private String email;
+
+    public Company() {
+    }
+
+    public Company(String companyID, String companyName, String information, String address, String contact, String phone, String email) {
+        this.companyID = companyID;
+        this.companyName = companyName;
+        this.information = information;
+        this.address = address;
+        this.contact = contact;
+        this.phone = phone;
+        this.email = email;
+    }
+
+
+    
+
+    /**
+     * @return the companyID
+     */
+    public String getCompanyID() {
+        return companyID;
+    }
+
+    /**
+     * @param companyID the companyID to set
+     */
+    public void setCompanyID(String companyID) {
+        this.companyID = companyID;
+    }
+
+    /**
+     * @return the companyName
+     */
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    /**
+     * @param companyName the companyName to set
+     */
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    /**
+     * @return the information
+     */
+    public String getInformation() {
+        return information;
+    }
+
+    /**
+     * @param information the information to set
+     */
+    public void setInformation(String information) {
+        this.information = information;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the contact
+     */
+    public String getContact() {
+        return contact;
+    }
+
+    /**
+     * @param contact the contact to set
+     */
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    
+
+}
---
+++ trunk/HotelSourceCode/HotelJob/src/Class/Job.java
@@ -0,0 +1,297 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class;
+
+import java.io.Serializable;
+
+
+/**
+ *
+ * @author quang
+ */
+public class Job implements Serializable{
+
+    
+    private String jobID;
+    private String jobName;
+    private String salary;
+    private String description;
+    private String required;
+    private String deadline;
+    private String domainID;
+    private String domainName;
+    private String number;
+    private String companyID;
+    private String companyName;
+    private String cityID;
+    private String cityName;
+    private String positionID;
+    private String positionName;
+    private String timeID;
+    private String timeInfo;
+
+    public Job() {
+    }
+
+    public Job(String jobID, String jobName, String salary, String description, String required, String deadline, String domainID, String domainName, String number, String companyID, String companyName, String cityID, String cityName, String positionID, String positionName, String timeID, String timeInfo) {
+        this.jobID = jobID;
+        this.jobName = jobName;
+        this.salary = salary;
+        this.description = description;
+        this.required = required;
+        this.deadline = deadline;
+        this.domainID = domainID;
+        this.domainName = domainName;
+        this.number = number;
+        this.companyID = companyID;
+        this.companyName = companyName;
+        this.cityID = cityID;
+        this.cityName = cityName;
+        this.positionID = positionID;
+        this.positionName = positionName;
+        this.timeID = timeID;
+        this.timeInfo = timeInfo;
+    }
+
+    /**
+     * @return the jobID
+     */
+    public String getJobID() {
+        return jobID;
+    }
+
+    /**
+     * @param jobID the jobID to set
+     */
+    public void setJobID(String jobID) {
+        this.jobID = jobID;
+    }
+
+    /**
+     * @return the jobName
+     */
+    public String getJobName() {
+        return jobName;
+    }
+
+    /**
+     * @param jobName the jobName to set
+     */
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    /**
+     * @return the salary
+     */
+    public String getSalary() {
+        return salary;
+    }
+
+    /**
+     * @param salary the salary to set
+     */
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the required
+     */
+    public String getRequired() {
+        return required;
+    }
+
+    /**
+     * @param required the required to set
+     */
+    public void setRequired(String required) {
+        this.required = required;
+    }
+
+    /**
+     * @return the deadline
+     */
+    public String getDeadline() {
+        return deadline;
+    }
+
+    /**
+     * @param deadline the deadline to set
+     */
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
+    /**
+     * @return the domainID
+     */
+    public String getDomainID() {
+        return domainID;
+    }
+
+    /**
+     * @param domainID the domainID to set
+     */
+    public void setDomainID(String domainID) {
+        this.domainID = domainID;
+    }
+
+    /**
+     * @return the domainName
+     */
+    public String getDomainName() {
+        return domainName;
+    }
+
+    /**
+     * @param domainName the domainName to set
+     */
+    public void setDomainName(String domainName) {
+        this.domainName = domainName;
+    }
+
+    /**
+     * @return the number
+     */
+    public String getNumber() {
+        return number;
+    }
+
+    /**
+     * @param number the number to set
+     */
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    /**
+     * @return the companyID
+     */
+    public String getCompanyID() {
+        return companyID;
+    }
+
+    /**
+     * @param companyID the companyID to set
+     */
+    public void setCompanyID(String companyID) {
+        this.companyID = companyID;
+    }
+
+    /**
+     * @return the companyName
+     */
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    /**
+     * @param companyName the companyName to set
+     */
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    /**
+     * @return the cityID
+     */
+    public String getCityID() {
+        return cityID;
+    }
+
+    /**
+     * @param cityID the cityID to set
+     */
+    public void setCityID(String cityID) {
+        this.cityID = cityID;
+    }
+
+    /**
+     * @return the cityName
+     */
+    public String getCityName() {
+        return cityName;
+    }
+
+    /**
+     * @param cityName the cityName to set
+     */
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    /**
+     * @return the positionID
+     */
+    public String getPositionID() {
+        return positionID;
+    }
+
+    /**
+     * @param positionID the positionID to set
+     */
+    public void setPositionID(String positionID) {
+        this.positionID = positionID;
+    }
+
+    /**
+     * @return the positionName
+     */
+    public String getPositionName() {
+        return positionName;
+    }
+
+    /**
+     * @param positionName the positionName to set
+     */
+    public void setPositionName(String positionName) {
+        this.positionName = positionName;
+    }
+
+    /**
+     * @return the timeID
+     */
+    public String getTimeID() {
+        return timeID;
+    }
+
+    /**
+     * @param timeID the timeID to set
+     */
+    public void setTimeID(String timeID) {
+        this.timeID = timeID;
+    }
+
+    /**
+     * @return the timeInfo
+     */
+    public String getTimeInfo() {
+        return timeInfo;
+    }
+
+    /**
+     * @param timeInfo the timeInfo to set
+     */
+    public void setTimeInfo(String timeInfo) {
+        this.timeInfo = timeInfo;
+    }
+
+    
+}
